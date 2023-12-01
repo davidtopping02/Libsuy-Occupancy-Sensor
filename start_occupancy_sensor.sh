9a11e36d2efa522fcb72c015caeccf36b68fa14b
David Topping <241905@dundee.ac.uk>
2023-12-01 09:46:54 +0000

    folder restructure

--- start_occupancy_sensor.sh
+++ start_occupancy_sensor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run the enable_monitoring_mode.sh script
-sudo bash networking_setup/enable-monitor-mode.sh
+sudo bash setup/networking_setup/enable-monitor-mode.sh
 
 # Run internet established script
-sudo /usr/bin/python3 networking_setup/internet_established.py
+sudo /usr/bin/python3 setup/networking_setup/internet_established.py
