4e6046a81d915eb5cba6a301497d703884ac32c5
David Topping <2412905@dundee.ac.uk>
2023-12-05 09:55:08 +0000

    added async and output of results

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 from occupancy_counting.package_scanner import PackageScanner
+from database_service.mariaDb import DatabaseManager
+import asyncio
 
+class OccupancySensor():
 
+    # Constructor to initialize the PackageScanner and DatabaseManager
+    def __init__(self):
+
+        self.network_traffic_scanner = PackageScanner("wlp2s0")
+        self.db_manager = DatabaseManager('occupancySensorLog.txt')
+
+    # Asynchronous method to get the current occupancy by scanning network traffic
+    async def getCurrentOccupancy(self):
+
+        scan_duration = 0.1
+        return await self.network_traffic_scanner.scan(scan_duration)
+
+    # Asynchronous method to run the lifecycle of the occupancy sensor
+    async def runSensorLifeCycle(self):
+        
+        while True:
+            scanTask = await sensor.getCurrentOccupancy()
+            print(scanTask)
+            # Appending a new line to the specified text file using the DatabaseManager
+            self.db_manager.append_to_file(str(scanTask))
+
+# Entry point of the script
 if __name__ == "__main__":
-    network_interface = "wlan1mon"
-    capture_duration_minutes = 10
 
     print("START")
-    try:
-        packet_sniffer = PackageScanner(network_interface, duration_minutes=capture_duration_minutes)
-        packet_sniffer.capture_packets()
-
-    except KeyboardInterrupt:
-        print("CAPTURE INTERRUPTED")
-
+    sensor = OccupancySensor()
+    asyncio.run(sensor.runSensorLifeCycle())
     print("END")
--- occupancy_counting/package_scanner.py
+++ occupancy_counting/package_scanner.py
@@ -1,16 +1,12 @@
-import threading
+import asyncio
 from scapy.all import sniff
 from collections import Counter
-import time
 
 class PackageScanner:
     
-    def __init__(self, interface, duration_minutes):
+    def __init__(self, interface):
         self.interface = interface
-        self.duration_minutes = duration_minutes
         self.unique_macs = Counter()
-        self.mac_counts_array = []
-        self.sniffing_flag = threading.Event()
 
     def packet_callback(self, packet):
         src_mac = None
@@ -19,46 +15,26 @@ class PackageScanner:
             src_mac = packet["Ethernet"].src
         elif packet.haslayer("Dot11"):
             src_mac = packet["Dot11"].addr2
-
         if src_mac:
             self.unique_macs[src_mac] += 1
 
-    def print_runtime(self, start_time):
-        while self.sniffing_flag.is_set():
-            current_time = time.time()
-            elapsed_time = current_time - start_time
-            formatted_time = time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
-            print(f"Elapsed time: [{formatted_time}]")
-            time.sleep(60)
+    async def sniff_packets(self, duration_minutes):
 
-    def sniff_packets(self):
-        self.sniffing_flag.set()
+        self.unique_macs.clear()
 
-        print(f"Starting sniff on {self.interface} for {self.duration_minutes} minutes")
-        packets = sniff(iface=self.interface, prn=self.packet_callback, timeout=60 * self.duration_minutes, store=0)
+        print(f"Starting sniff on {self.interface} for {duration_minutes} minutes")
+        packets = sniff(iface=self.interface, prn=self.packet_callback, timeout=60*duration_minutes, store=0)
         print("Finished sniffing")
 
         print("Calculating unique MACs")
-        num_unique_macs = len(self.unique_macs)
+        mac_count = len(self.unique_macs)
 
-        print(f"Number of Unique MAC Addresses: {num_unique_macs}")
-        self.mac_counts_array.append(num_unique_macs)
-        self.unique_macs.clear()
+        return mac_count
 
-        self.sniffing_flag.clear()
 
-    def capture_packets(self):
-        while True:
-            start_time = time.time()
 
-            # Run sniff asynchronously
-            sniffer_thread = threading.Thread(target=self.sniff_packets)
-            sniffer_thread.start()
+    async def scan(self, scan_duration):
+        
+        mac_count = await self.sniff_packets(scan_duration)
 
-            # Run print_runtime asynchronously
-            timer_thread = threading.Thread(target=self.print_runtime, args=(start_time,))
-            timer_thread.start()
-
-            # Wait for the sniffing thread to complete
-            sniffer_thread.join()
-            timer_thread.join()
+        return mac_count
