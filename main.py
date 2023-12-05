463e3dc7dc8d246c9a149eee7e068eb5ee35a44c
David Topping <2412905@dundee.ac.uk>
2023-12-05 10:18:34 +0000

    added time to the output

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 from occupancy_counting.package_scanner import PackageScanner
 from database_service.mariaDb import DatabaseManager
 import asyncio
+import datetime
 
 class OccupancySensor():
 
     # Constructor to initialize the PackageScanner and DatabaseManager
     def __init__(self):
 
-        self.network_traffic_scanner = PackageScanner("wlp2s0")
+        self.network_traffic_scanner = PackageScanner("wlan1mon")
         self.db_manager = DatabaseManager('occupancySensorLog.txt')
 
     # Asynchronous method to get the current occupancy by scanning network traffic
     async def getCurrentOccupancy(self):
 
-        scan_duration = 0.1
+        scan_duration = 20
         return await self.network_traffic_scanner.scan(scan_duration)
 
-    # Asynchronous method to run the lifecycle of the occupancy sensor
     async def runSensorLifeCycle(self):
-        
         while True:
             scanTask = await sensor.getCurrentOccupancy()
-            print(scanTask)
-            # Appending a new line to the specified text file using the DatabaseManager
-            self.db_manager.append_to_file(str(scanTask))
+            
+            # Get the current date and time
+            current_datetime = datetime.datetime.now()
+            formatted_datetime = current_datetime.strftime('%H:%M:%S')
+            
+            # Combine date, time, and scanTask with commas and output it
+            output_line = f"{formatted_datetime},{scanTask}"
+            print(output_line)
+            self.db_manager.append_to_file(output_line)
 
 # Entry point of the script
 if __name__ == "__main__":
