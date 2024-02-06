1cd4ec65ff8ed78f7d56642232074f5757744cf2
davidtopping02 <241905@dundee.ac.uk>
2024-02-06 10:35:49 +0000

    changed custom scripts

--- scripts/enable-monitoring-mode.sh
+++ scripts/enable-monitoring-mode.sh
@@ -27,4 +27,15 @@ else
     exit 1
 fi
 
+# Get the parent directory of the current directory
+parent_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+
+# Check if main.py exists in the parent directory
+if [ -e "$parent_directory/main.py" ]; then
+    # Run main.py with sudo
+    python3 "$parent_directory/main.py"
+else
+    echo "main.py not found in the parent directory."
+fi
+
 exit 0
